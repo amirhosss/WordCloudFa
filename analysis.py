7e6455b091c5b3bddbc62cf7f724a13abcc402d4
Amirhosss <amirhosseinleo@yahoo.com>
2022-08-05 18:47:43 +0430

    First commit

---
+++ analysis.py
@@ -0,0 +1,105 @@
+import re
+import json
+import codecs
+
+import numpy as np
+import arabic_reshaper
+from wordcloud import WordCloud
+from bidi.algorithm import get_display
+
+
+class WordCloudFa():
+    def __init__(self, wc: WordCloud, stopwords_file: str, mask_pixel: int) -> None:
+        self.wc = wc
+        self.stopwords_file = stopwords_file
+        self.mask_pixel = mask_pixel
+
+    def rewrite_stopwords(self) -> set:
+        stopwords = set()
+
+        stop = codecs.open(self.stopwords_file, 'r', 'utf-8')
+        lines = stop.readlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i].replace('ي', 'ی')
+        stop = codecs.open(self.stopwords_file, 'w', 'utf-8').write(''.join(lines))
+        stop = codecs.open(self.stopwords_file, 'r', 'utf-8')
+
+        for line in stop:
+            line = line.rstrip()
+            stopword = arabic_reshaper.reshape(line)
+            stopword = get_display(stopword)
+            stopwords.add(stopword)
+
+        return stopwords
+
+    def preprocessing_text(self, text: str) -> str:
+        weridPatterns = re.compile(
+            "["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            u"\U00002702-\U000027B0"
+            u"\U000024C2-\U0001F251"
+            u"\U0001f926-\U0001f937"
+            u'\U00010000-\U0010ffff'
+            u"\u200d"
+            u"\u2640-\u2642"
+            u"\u2600-\u2B55"
+            u"\u23cf"
+            u"\u23e9"
+            u"\u231a"
+            u"\u3030"
+            u"\ufe0f"
+            u"\u2069"
+            u"\u2066"
+            u"\u200c"
+            u"\u2068"
+            u"\u2067"
+            'A-z'
+            '@'
+            '//'
+            "]+", flags=re.UNICODE
+        )
+        clean_text = weridPatterns.sub(r'', text)
+        codecs.open('text.txt', 'w', 'utf-8').write(clean_text)
+
+        return clean_text
+
+    def generate(self, text: str, save_file_name: str) -> None:
+        text = get_display(arabic_reshaper.reshape(text))
+
+        self.wc.stopwords = self.rewrite_stopwords()
+
+        x, y = np.ogrid[:self.mask_pixel, :self.mask_pixel]
+
+        mask = ((x-self.mask_pixel/2) ** 2 + (y-self.mask_pixel/2) ** 2 > 
+        (.45*self.mask_pixel) ** 2)
+        mask = 255 * mask.astype(int)
+        self.wc.mask = mask
+
+        self.wc.generate(text)
+        image = self.wc.to_image()
+        image.show()
+        image.save(save_file_name)
+
+
+if __name__ == '__main__':
+    file_name = ''
+    with open(f'{file_name}.json', 'r', encoding='utf-8') as file:
+        tweets = json.loads(file.read())
+
+    all_tweets = []
+    for tweet in tweets:
+        all_tweets.append(tweet['text'])
+
+    text = '\n'.join(all_tweets)
+
+    wc = WordCloud(
+        font_path='fonts/Shabnam/Shabnam.ttf',
+        background_color='white',
+        collocations=False,
+    )
+    wcfa = WordCloudFa(wc=wc, stopwords_file='stop.txt', mask_pixel=1000)
+    text = wcfa.preprocessing_text(text)
+    wcfa.generate(text=text, save_file_name=f'{file_name}.png')
---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import json
+import itertools
+from dataclasses import dataclass
+
+import requests as req
+from dotenv import load_dotenv
+
+
+@dataclass
+class Client():
+    username_url: str
+    tweet_url: str
+    bearer_token: str
+    proxy: dict
+
+
+class User():
+    def __init__(self, username) -> None:
+        self.username = username
+    
+    def get_id(self, client: Client) -> int:
+        try:
+            res = req.get(client.username_url+self.username, headers={'Authorization': client.bearer_token},
+            proxies=client.proxy)
+            data = res.json()
+            self.user_id = data['data']['id']
+
+            return self.user_id
+
+        except Exception as ex:
+            print(f'An error is occurred {ex}')
+
+    def get_tweets(self, client: Client, params: dict):
+        self.get_id(client)
+        data = []
+
+        while True:
+            try:
+                res = req.get(client.tweet_url.format(user_id=self.user_id), headers={'Authorization': client.bearer_token},
+                params=params, proxies=client.proxy)
+
+                data.append(res.json()['data'])
+                meta = res.json()['meta']
+
+                if meta.get('next_token') is None:
+                    break
+
+                params.update({'pagination_token': meta['next_token']})
+
+            except Exception as ex:
+                print(f'An error is occurred {ex}')
+                
+        return data
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    TOKEN = os.environ.get('TOKEN')
+
+    my_client = Client(
+    username_url='https://api.twitter.com/2/users/by/username/',
+    tweet_url='https://api.twitter.com/2/users/{user_id}/tweets',
+    bearer_token=TOKEN,
+    proxy={'https': 'http://127.0.0.1:10809'}
+    )
+    user = User('')
+
+    data = list(itertools.chain(*user.get_tweets(my_client, 
+    params={'max_results': 100})))
+
+    with open(f'{user.username}.json', 'w') as file:
+        file.write(json.dumps(data, indent=0))
